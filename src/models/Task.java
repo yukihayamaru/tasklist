736f3a666b01882ec4d13c3d1ca844a1392bd023
hayamaruhiroshiakira <hayamaruhiroshiakira@192.168.0.23>
2021-12-10 22:18:45 +0900

    まとめてコミット

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,22 +7,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
-public class tasks{
+public class Task{
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
-
-    @Column(name = "content", length = 255, nullable = false)
+    @Column(name = "content",length = 255, nullable = false)
     private String content;
-
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
@@ -37,16 +39,15 @@ public class tasks{
         this.id = id;
     }
 
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
         this.content = content;
-    }
-
-    public Timestamp getCreated_at() {
-        return created_at;
     }
 
     public void setCreated_at(Timestamp created_at) {
